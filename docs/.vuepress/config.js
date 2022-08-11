88635e7c2042e7e1816d37fefee953b8a931dcce
史泰龙 <190221@lifeat.cn>
2022-08-11 10:52:14 +0800

    feat:finish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,16 @@
 module.exports = {
+	title: "V-Widget",
 	base: "/v-wigget/",
 	themeConfig: {
+		nav: [
+			{
+				text: "Home", link: "/"
+			}, {
+				text: "Github", link: "https://github.com/stalllone/v-wigget"
+			}, {
+				text: "Npm", link: "https://www.npmjs.com/package/v-widget"
+			}
+		],
 		sidebar: [
 			'/',
 			'/componentDocs/card'
